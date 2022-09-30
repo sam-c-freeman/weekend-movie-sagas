635f71bd07fbec1fe81a056aa742033a17659b07
Sam Freeman <samcfreeman888@gmail.com>
2022-09-30 16:59:21 -0500

    on router almost ready to send data to reducer

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,13 +1,41 @@
 import {useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
+import Card from '@mui/material/Card';
 
 
 function DetailsPage (){
-  //back button?  
+    const params = useParams();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const movieId = params.id
     
+    useEffect(() => {
+        
+        dispatch ({
+            type: 'FETCH_MOVIE_DETAILS',
+            payload: movieId
+        })
+    }, [movieId])
+    
+    const backToHome = () =>{
+        history.push('/')
+  }
+    
+
+  //why is button in header?
     return(
-        <p>hi</p>
+        <>
+            <button onClick={backToHome}>Home</button>
+      
+            <Card>
+                <p>hi</p>
+                <p>hi</p>
+                <p>hi</p>
+            </Card>
+        </>
     )
 }
 
